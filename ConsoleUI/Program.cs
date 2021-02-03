2bdc5175bd16313b062326f7a29cb6db65336612
Ramazan GÜRSOY <gursoy90@gmail.com>
2021-02-03 23:50:52 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Concrete.InMemory;
+using Business.Concrete;
+using System;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleUI
+{
+    //SOLID
+    //Open Closed Principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(10,20))
+            {
+               Console.WriteLine(product.ProductName);
+            }
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //NuGet
+    public class EfCategoryDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            using (NortwindContext context = new NortwindContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
